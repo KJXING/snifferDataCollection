e4c441f1c58b5f8cf265a2cf73960362f119ece6
Ke Junxing <csjxke@comp-dept-ad2.comp.hkbu.edu.hk>
2020-07-15 18:00:32 +0800

    first commit

---
+++ snifferNodeSimulator.py
@@ -0,0 +1,15 @@
+import socket
+
+
+
+def main():
+    sniffer_node = 5
+    target_devices_mac = []
+    sniffer_interface_mac = []
+    sniffer_channel = ['2412']
+
+
+
+
+if __name__ == '__main__':
+    main()
